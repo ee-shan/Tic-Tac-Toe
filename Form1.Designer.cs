ab0c71d320767fe2066d09fb791c29f328f88458
Azizul Hakim Eshan <eshazizul@gmail.com>
2021-04-17 17:56:12 +0600

    almost done version

---
+++ Form1.Designer.cs
@@ -0,0 +1,361 @@
+﻿
+namespace Whiteboard_Windows_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b00 = new System.Windows.Forms.Button();
+            this.b01 = new System.Windows.Forms.Button();
+            this.b02 = new System.Windows.Forms.Button();
+            this.b10 = new System.Windows.Forms.Button();
+            this.b11 = new System.Windows.Forms.Button();
+            this.b12 = new System.Windows.Forms.Button();
+            this.b20 = new System.Windows.Forms.Button();
+            this.b21 = new System.Windows.Forms.Button();
+            this.b22 = new System.Windows.Forms.Button();
+            this.btn_restart = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.playerX = new System.Windows.Forms.TextBox();
+            this.playerO = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // b00
+            // 
+            this.b00.BackColor = System.Drawing.Color.Tan;
+            this.b00.Font = new System.Drawing.Font("Consolas", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b00.Location = new System.Drawing.Point(52, 232);
+            this.b00.Name = "b00";
+            this.b00.Size = new System.Drawing.Size(100, 95);
+            this.b00.TabIndex = 0;
+            this.b00.UseVisualStyleBackColor = false;
+            this.b00.Click += new System.EventHandler(this.button_click);
+            // 
+            // b01
+            // 
+            this.b01.BackColor = System.Drawing.Color.Tan;
+            this.b01.Font = new System.Drawing.Font("Consolas", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b01.Location = new System.Drawing.Point(174, 232);
+            this.b01.Name = "b01";
+            this.b01.Size = new System.Drawing.Size(100, 95);
+            this.b01.TabIndex = 1;
+            this.b01.UseVisualStyleBackColor = false;
+            this.b01.Click += new System.EventHandler(this.button_click);
+            // 
+            // b02
+            // 
+            this.b02.BackColor = System.Drawing.Color.Tan;
+            this.b02.Font = new System.Drawing.Font("Consolas", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b02.Location = new System.Drawing.Point(296, 232);
+            this.b02.Name = "b02";
+            this.b02.Size = new System.Drawing.Size(100, 95);
+            this.b02.TabIndex = 2;
+            this.b02.UseVisualStyleBackColor = false;
+            this.b02.Click += new System.EventHandler(this.button_click);
+            // 
+            // b10
+            // 
+            this.b10.BackColor = System.Drawing.Color.Tan;
+            this.b10.Font = new System.Drawing.Font("Consolas", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b10.Location = new System.Drawing.Point(52, 343);
+            this.b10.Name = "b10";
+            this.b10.Size = new System.Drawing.Size(100, 95);
+            this.b10.TabIndex = 3;
+            this.b10.UseVisualStyleBackColor = false;
+            this.b10.Click += new System.EventHandler(this.button_click);
+            // 
+            // b11
+            // 
+            this.b11.BackColor = System.Drawing.Color.Tan;
+            this.b11.Font = new System.Drawing.Font("Consolas", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b11.Location = new System.Drawing.Point(174, 343);
+            this.b11.Name = "b11";
+            this.b11.Size = new System.Drawing.Size(100, 95);
+            this.b11.TabIndex = 4;
+            this.b11.UseVisualStyleBackColor = false;
+            this.b11.Click += new System.EventHandler(this.button_click);
+            // 
+            // b12
+            // 
+            this.b12.BackColor = System.Drawing.Color.Tan;
+            this.b12.Font = new System.Drawing.Font("Consolas", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b12.Location = new System.Drawing.Point(296, 343);
+            this.b12.Name = "b12";
+            this.b12.Size = new System.Drawing.Size(100, 95);
+            this.b12.TabIndex = 5;
+            this.b12.UseVisualStyleBackColor = false;
+            this.b12.Click += new System.EventHandler(this.button_click);
+            // 
+            // b20
+            // 
+            this.b20.BackColor = System.Drawing.Color.Tan;
+            this.b20.Font = new System.Drawing.Font("Consolas", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b20.Location = new System.Drawing.Point(52, 454);
+            this.b20.Name = "b20";
+            this.b20.Size = new System.Drawing.Size(100, 95);
+            this.b20.TabIndex = 6;
+            this.b20.UseVisualStyleBackColor = false;
+            this.b20.Click += new System.EventHandler(this.button_click);
+            // 
+            // b21
+            // 
+            this.b21.BackColor = System.Drawing.Color.Tan;
+            this.b21.Font = new System.Drawing.Font("Consolas", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b21.Location = new System.Drawing.Point(174, 454);
+            this.b21.Name = "b21";
+            this.b21.Size = new System.Drawing.Size(100, 95);
+            this.b21.TabIndex = 7;
+            this.b21.UseVisualStyleBackColor = false;
+            this.b21.Click += new System.EventHandler(this.button_click);
+            // 
+            // b22
+            // 
+            this.b22.BackColor = System.Drawing.Color.Tan;
+            this.b22.Font = new System.Drawing.Font("Consolas", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b22.Location = new System.Drawing.Point(296, 454);
+            this.b22.Name = "b22";
+            this.b22.Size = new System.Drawing.Size(100, 95);
+            this.b22.TabIndex = 8;
+            this.b22.UseVisualStyleBackColor = false;
+            this.b22.Click += new System.EventHandler(this.button_click);
+            // 
+            // btn_restart
+            // 
+            this.btn_restart.BackColor = System.Drawing.Color.ForestGreen;
+            this.btn_restart.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_restart.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_restart.Location = new System.Drawing.Point(322, 649);
+            this.btn_restart.Name = "btn_restart";
+            this.btn_restart.Size = new System.Drawing.Size(135, 50);
+            this.btn_restart.TabIndex = 9;
+            this.btn_restart.Text = "RESTART";
+            this.btn_restart.UseVisualStyleBackColor = false;
+            this.btn_restart.Click += new System.EventHandler(this.btn_restart_Click);
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn_exit.Font = new System.Drawing.Font("Consolas", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_exit.ForeColor = System.Drawing.Color.Crimson;
+            this.btn_exit.Location = new System.Drawing.Point(322, 20);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(135, 50);
+            this.btn_exit.TabIndex = 10;
+            this.btn_exit.Text = "EXIT";
+            this.btn_exit.UseVisualStyleBackColor = false;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // playerX
+            // 
+            this.playerX.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playerX.Location = new System.Drawing.Point(16, 153);
+            this.playerX.Name = "playerX";
+            this.playerX.Size = new System.Drawing.Size(150, 32);
+            this.playerX.TabIndex = 11;
+            this.playerX.Text = "X";
+            this.playerX.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // playerO
+            // 
+            this.playerO.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playerO.Location = new System.Drawing.Point(298, 153);
+            this.playerO.Name = "playerO";
+            this.playerO.Size = new System.Drawing.Size(150, 32);
+            this.playerO.TabIndex = 12;
+            this.playerO.Text = "O";
+            this.playerO.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Brown;
+            this.label1.Location = new System.Drawing.Point(16, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 26);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Nick Name (X)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Brown;
+            this.label2.Location = new System.Drawing.Point(280, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 26);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Nick Name (O)";
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Font = new System.Drawing.Font("Consolas", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result.ForeColor = System.Drawing.Color.Crimson;
+            this.result.Location = new System.Drawing.Point(51, 579);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(379, 43);
+            this.result.TabIndex = 15;
+            this.result.Text = "Game is Running...";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.GrayText;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(52, 330);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(344, 10);
+            this.label3.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.GrayText;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(52, 441);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(344, 10);
+            this.label4.TabIndex = 17;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.SystemColors.GrayText;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(158, 232);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(10, 317);
+            this.label5.TabIndex = 18;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.SystemColors.GrayText;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label6.Location = new System.Drawing.Point(280, 232);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(10, 317);
+            this.label6.TabIndex = 19;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Brown;
+            this.label7.Location = new System.Drawing.Point(12, 46);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(190, 24);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "X goes first...";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Brown;
+            this.label8.Location = new System.Drawing.Point(12, 87);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(216, 19);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "Enter player name below";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(469, 711);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.playerO);
+            this.Controls.Add(this.playerX);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_restart);
+            this.Controls.Add(this.b22);
+            this.Controls.Add(this.b21);
+            this.Controls.Add(this.b20);
+            this.Controls.Add(this.b12);
+            this.Controls.Add(this.b11);
+            this.Controls.Add(this.b10);
+            this.Controls.Add(this.b02);
+            this.Controls.Add(this.b01);
+            this.Controls.Add(this.b00);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Tic-Tac-Toe!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b00;
+        private System.Windows.Forms.Button b01;
+        private System.Windows.Forms.Button b02;
+        private System.Windows.Forms.Button b10;
+        private System.Windows.Forms.Button b11;
+        private System.Windows.Forms.Button b12;
+        private System.Windows.Forms.Button b20;
+        private System.Windows.Forms.Button b21;
+        private System.Windows.Forms.Button b22;
+        private System.Windows.Forms.Button btn_restart;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.TextBox playerX;
+        private System.Windows.Forms.TextBox playerO;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Whiteboard_Windows_Form
+{
+    public partial class Form1 : Form
+    {
+        /*
+            X => turn is false
+            O => turn is true
+        */
+
+        bool turn = true;
+        int turn_count = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+
+            if (turn) b.Text = "X";
+            else b.Text = "O";
+
+            turn = !turn;
+            b.Enabled = false;
+
+            turn_count++;
+
+            win_checker();
+        }
+
+        private void win_checker()
+        {
+            bool finished = false;
+
+            if ((b00.Text == b01.Text) && (b01.Text == b02.Text) && (!b00.Enabled))
+                finished = true;
+            else if ((b10.Text == b11.Text) && (b11.Text == b12.Text) && (!b10.Enabled))
+                finished = true;
+            else if ((b20.Text == b21.Text) && (b21.Text == b22.Text) && (!b20.Enabled))
+                finished = true;
+            else if ((b00.Text == b10.Text) && (b10.Text == b20.Text) && (!b00.Enabled))
+                finished = true;
+            else if ((b01.Text == b11.Text) && (b11.Text == b21.Text) && (!b01.Enabled))
+                finished = true;
+            else if ((b02.Text == b12.Text) && (b12.Text == b22.Text) && (!b02.Enabled))
+                finished = true;
+            else if ((b00.Text == b11.Text) && (b11.Text == b22.Text) && (!b00.Enabled))
+                finished = true;
+            else if ((b02.Text == b11.Text) && (b11.Text == b20.Text) && (!b20.Enabled))
+                finished = true;
+
+            if (finished)
+            {
+                disable_button();
+
+                string winner = "";
+
+                if (turn) winner = playerO.Text;
+                else winner = playerX.Text;
+
+                result.Text = winner + " Won!";
+            }
+            else
+            {
+                if (turn_count == 9)
+                    result.Text = "Game Drawn...";
+            }
+        }
+
+        private void disable_button()
+        {
+            try
+            {
+                foreach (Control control in Controls)
+                {
+                    if (control is Button)
+                    {
+                        Button b = (Button)control;
+
+                        if (b != btn_exit && b != btn_restart)
+                            b.Enabled = false;
+                    }
+                }
+            }
+            catch { }
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btn_restart_Click(object sender, EventArgs e)
+        {
+            result.Text = "Game is Running...";
+
+            turn = true;
+            turn_count = 0;
+
+            try
+            {
+                foreach (Control c in Controls)
+                {
+                    if (c is Button)
+                    {
+                        Button b = (Button)c;
+                        b.Enabled = true;
+
+                        if (b != btn_exit && b != btn_restart)
+                            b.Text = "";
+                    }
+                }
+            }
+            catch { }
+        }
+    }
+}
